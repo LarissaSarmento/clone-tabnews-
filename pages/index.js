4716046808d0de7fb4e5baf9ae2ec16caca453ca
LarissaSarmento <larissalicurgoaltoe@outlook.com>
2024-08-21 17:03:49 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home(){
-    return <h1>Lucas eu te amo. Se você me ama, dá uma risadinha!</h1>
+    return <h1>Meu bem Lucas eu te amo. Se você me ama, dá uma risadinha!!!</h1>
 }
 export default Home;
