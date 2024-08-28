878bbf0d1828f5437c32529d0deae86e77f50a09
LarissaSarmento <larissalicurgoaltoe@outlook.com>
2024-08-28 00:27:25 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
-function Home(){
-    return <h1>Mãe eu te amo. Se você me ama, dá uma risadinha!!!</h1>
+function Home() {
+  return <h1>Mãe eu te amo. Se você me ama, dá uma risadinha!!!</h1>;
 }
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
+}
+
 export default Home;
